0c429c9c566a10a513095785c2a08623b8a25e0f
Nicholas Chu <nchu999@gmail.com>
2020-08-11 18:33:33 -0500

    Refactor fetch prices service

--- src/services/finance-data.service.ts
+++ src/services/finance-data.service.ts
@@ -3,8 +3,6 @@ import { FinanceDataFetchPriceException } from '../exceptions/finance-data-fetch
 import { YahooFinanceQuoteResponse } from '../models/yahoo-finance-quote-response';
 
 export class FinanceDataService {
-  private quoteEndpoint = 'https://query2.finance.yahoo.com/v7/finance/quote';
-
   /**
    * Fetch the prices of a list of stocks
    *
@@ -12,9 +10,16 @@ export class FinanceDataService {
    * @return A list of prices
    */
   async fetchPrices(symbols: string[]): Promise<number[]> {
-    const joinedSymbols = symbols.join();
-    const quoteUrl = `${this.quoteEndpoint}?symbols=${joinedSymbols}&fields=regularMarketPrice`;
-    const response = await axios.get(quoteUrl);
+    const quoteEndpoint = 'https://query2.finance.yahoo.com/v7/finance/quote';
+    const joinedSymbols: string = symbols.join();
+
+    const response = await axios.get(quoteEndpoint, {
+      params: {
+        symbols: joinedSymbols,
+        fields: 'regularMarketPrice',
+      },
+    });
+
     const quotes: YahooFinanceQuoteResponse[] =
       response.data.quoteResponse.result;
 
