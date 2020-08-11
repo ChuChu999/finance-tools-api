e18c973098fbcf5142d80aaae4bad66c2b22d41b
Nicholas Chu <nchu999@gmail.com>
2020-08-11 18:52:30 -0500

    Add return type to function

--- src/middleware/async-handler.middleware.ts
+++ src/middleware/async-handler.middleware.ts
@@ -2,7 +2,7 @@ import { NextFunction, Request, Response } from 'express';
 
 export function asyncHandlerMiddleware(
   fn: (req: Request, res: Response, next: NextFunction) => Promise<void>
-) {
+): (req: Request, res: Response, next: NextFunction) => void {
   return (req: Request, res: Response, next: NextFunction): void => {
     fn(req, res, next).catch(next);
   };
